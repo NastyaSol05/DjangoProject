c8c2d1c04222d25904de51453a6a9291fb07b548
Nastya Sol <an5oldatova@ya.ru>
2024-12-05 12:43:05 +0300

    add new functions

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price')
+    list_filter = ('category', )
+    search_fields = ('name', 'description', )
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,8 +54,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "django_project",
+        "USER": "postgres",
+        "PASSWORD": "anastaA05",
+        "HOST": "localhost",
+        "PORT": "5432",  # default postgres port
     }
 }
 
@@ -90,3 +95,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
